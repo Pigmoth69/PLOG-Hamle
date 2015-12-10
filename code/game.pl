13b490341a89dd5e85d2d296e8cb77ea66479f48
guilhermevpinto <guilhermevpinto@gmail.com>
2015-12-10 15:17:45 +0000

    not working :c

--- code/game.pl
+++ code/game.pl
@@ -26,36 +26,118 @@ getBlackCells([Head | Tail], [Head | Blacklist]):-
 	getBlackCells(Tail, Blacklist).
 
 
+%------------------------------------%
+%--------------SOLUTION--------------%
+%------------------------------------%
+solution(Values, NumberWhites, N, Result):-
+	length(Values, Length),
+	length(Result, Length),
+	setUpBoard(Values, N, ValuesBoard),
+	setUpBoard(Result, N, ResultBoard),
+	domain(Result, 0, 4),
+
+	count(0, Result, #=, NumberWhites),
+	blacksNotAdjacent(ResultBoard),
+	blacksMovement(Values, Result, N, Length),
+
+	print(Result),nl,
+	labeling([], Result).
+
+
 
 %------------------------------------%
 %------------RESTRICTIONS------------%
 %------------------------------------%
 
-solution(Values, NumberWhites, N, Result):-
-	length(Values, Length),
-	length(Result, Length),
-	domain(Result, 0, 4),
 
-	count(0, Result, #=, NumberWhites),
-	blacksNotAdjacent(Result, N),                          
-
-	print(Result),nl,
-	labeling([], Result).
-
-blacksNotAdjacent(Result, N):-
-	setUpBoard(Result, N, ResultBoard),
+%*************************************%
+%*****BLACKS ADJACENCY CONSTRAIN******%
+%*************************************%
+blacksNotAdjacent(ResultBoard):-
 	transpose(ResultBoard, TResultBoard),
 	checkAdjacentCells(TResultBoard),
 	checkAdjacentCells(ResultBoard).
 
 checkAdjacentCells([]).
 checkAdjacentCells([Head | Tail]):-
-	checkAdjacentCellsRow(Head),
+	checkAdjacentCellsLine(Head),
 	checkAdjacentCells(Tail).
 
-checkAdjacentCellsRow([_ | []]).
-checkAdjacentCellsRow([First, Second | Tail]):-
-	First #>0, Second #= 0,
-	checkAdjacentCellsRow([Second | Tail]).
-checkAdjacentCellsRow([_ | Tail]):-
-	checkAdjacentCellsRow(Tail).
+
+checkAdjacentCellsLine([_ | []]).
+checkAdjacentCellsLine([First, Second | Tail]):-
+	First #>0 #=> Second #= 0,
+	checkAdjacentCellsLine([Second | Tail]).
+checkAdjacentCellsLine([_ | Tail]):-
+	checkAdjacentCellsLine(Tail).
+
+
+%***************************************%
+%*****BLACKS MOVEMENTS CONSTRAIN********%
+%***************************************%
+blacksMovement(_, _, _, 0).
+blacksMovement(Values, Result, N, Position):-
+	element(Position, Values, Value), Value #> 0 #=> moveBlack(Result, Position, Value, N),
+	NextPosition is Position - 1,
+	blacksMovement(Values, Result, N, NextPosition).
+blacksMovement(Values, Result, N, Position):-
+	NextPosition is Position - 1,
+	blacksMovement(Values, Result, N, NextPosition).
+
+moveBlack(Result, Position, Value, N):-
+	NewPosition #= Position - N*Value,
+	element(NewPosition, Result, 1).
+moveBlack(Result, Position, Value, N):-
+	NewPosition #= Position + N*Value,
+	element(NewPosition, Result, 2).
+moveBlack(Result, Position, Value, N):-
+	NewPosition #= Position - Value,
+	element(NewPosition, Result, 3).
+moveBlack(Result, Position, Value, N):-
+	NewPosition #= Position + Value,
+	element(NewPosition, Result, 4).
+
+/*
+blacksMovement(ResultBoard, ValuesBoard):-
+	checkHorizontalMovement(ValuesBoard, ResultBoard).
+blacksMovement(ResultBoard, ValuesBoard):-
+	transpose(ResultBoard, TResultBoard),
+	transpose(ValuesBoard, TValuesBoard),
+	checkVerticalMovement(TValuesBoard, TResultBoard).
+
+checkHorizontalMovement([], _).
+checkHorizontalMovement([Head | Tail], [RHead | RTail]):-
+	is_list(Head),
+	checkHorizontalMovement(Head, RHead),
+	checkHorizontalMovementLine(Tail, RTail).
+%esquerda
+checkHorizontalMovementLine(Values, Result):-
+	element(Position, Values, Value),
+	Value #> 0,
+	NewPosition #= Position - Value,
+	element(NewPosition, Result, 3).
+%direita
+checkHorizontalMovementLine(Values, Result):-
+	element(Position, Values, Value),
+	Value #> 0,
+	NewPosition #= Position + Value,
+	element(NewPosition, Result, 4).
+
+checkVerticalMovement([], _).
+checkVerticalMovement([Head | Tail], [RHead | RTail]):-
+	is_list(Head),
+	checkVerticalMovementLine(Head, RHead),
+	checkVerticalMovement(Tail, RTail).
+%cima
+checkVerticalMovementLine(Values, Result):-
+	element(Position, Values, Value),
+	Value #> 0,
+	NewPosition #= Position - Value,
+	element(NewPosition, Result, 1).
+%baixo
+checkVerticalMovementLine(Values, Result):-
+	element(Position, Values, Value),
+	Value #> 0,
+	NewPosition #= Position + Value,
+	element(NewPosition, Result, 2).
+	*/
