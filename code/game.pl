b15c90a0ee25952a42a2ce8dd06f7d0a6ff07b5b
F0lha <pedrovieiracastro@hotmail.com>
2015-12-10 17:19:53 +0000

    apaga os prints seu nabo

--- code/game.pl
+++ code/game.pl
@@ -1,4 +1,5 @@
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
 
 defaultGame:- 
 	defaultInfoList(Info),
@@ -34,11 +35,12 @@ solution(Values, NumberWhites, N, Result):-
 	length(Result, Length),
 	setUpBoard(Values, N, ValuesBoard),
 	setUpBoard(Result, N, ResultBoard),
-	domain(Result, 0, 4),
+	domain(Result, 0, 5),
 
 	count(0, Result, #=, NumberWhites),
+	
+	restrictMovements(Values,NumberWhites,N,Result),
 	blacksNotAdjacent(ResultBoard),
-	blacksMovement(Values, Result, N, Length),
 
 	print(Result),nl,
 	labeling([], Result).
@@ -75,9 +77,69 @@ checkAdjacentCellsLine([_ | Tail]):-
 %***************************************%
 %*****BLACKS MOVEMENTS CONSTRAIN********%
 %***************************************%
+
+createTuples(0,[]).
+
+createTuples(NumberBlacks,[L|ListTuples]):- NumberBlacks> 0,N1 is NumberBlacks - 1, length(L,1),createTuples(N1,ListTuples).
+
+checkIfRight(Index-Value,N,[Dest]):-OriginalFloor is floor((Index-1) / N),
+									Dest is (Index + Value),
+									DestFloor is floor((Dest-1) / N),
+									DestFloor =:= OriginalFloor.
+							
+
+checkIfRight(_,_,[]).
+
+checkIfLeft(Index-Value,N,[Dest]):-OriginalFloor is floor((Index-1) / N),
+									Dest is (Index - Value),
+									DestFloor is floor((Dest-1)/N),
+									DestFloor =:= OriginalFloor.
+							
+
+checkIfLeft(_,_,[]).
+
+checkIfUp(Index-Value,N,[Dest]):-Dest is (Index - Value*N),
+									Dest >= 1.
+									
+checkIfUp(_,_,[]).
+
+checkIfDown(Index-Value,N,[Dest]):-Dest is (Index + Value*N),
+									Dest =< (N*N).
+									
+checkIfDown(_,_,[]).
+
+tablingTuples([],[],_).
+
+tablingTuples([Tuple|ListTuples],[Piece|ListBlacks],N):-
+									checkIfRight(Piece,N,Dest1),
+									checkIfLeft(Piece,N,Dest2),
+									checkIfUp(Piece,N,Dest3),
+									checkIfDown(Piece,N,Dest4),
+									table([[Tuple]],[Dest1,Dest2,Dest3,Dest4]),
+									tablingTuples(ListTuples,ListBlacks,N).
+
+
+putPiecesInBoard([],[],_):-print('aqui').									
+									
+putPiecesInBoard([Tuple|ListTuples],[_-Value | ListBlacks],Result):-element(Tuple,Result,Value),
+																	putPiecesInBoard(ListTuples, ListBlacks ,Result).
+
+
+
+restrictMovements(Board,NumberWhites,N,Result):-
+				NumberBlacks is (N*N)-NumberWhites,
+				length(ListTuples,NumberBlacks),
+				findall(Index-Value,(nth1(Index,Board,Value),Value>0),ListBlacks),
+				tablingTuples(ListTuples,ListBlacks,N),
+				all_different(ListTuples),
+				putPiecesInBoard(ListTuples,ListBlacks,Result).
+
+
+
+
 blacksMovement(_, _, _, 0).
 blacksMovement(Values, Result, N, Position):-
-	element(Position, Values, Value), Value #> 0 #=> moveBlack(Result, Position, Value, N),
+	element(Position, Values, Value), Value > 0, moveBlack(Result, Position, Value, N),write('Chega aqui'),
 	NextPosition is Position - 1,
 	blacksMovement(Values, Result, N, NextPosition).
 blacksMovement(Values, Result, N, Position):-
@@ -85,17 +147,21 @@ blacksMovement(Values, Result, N, Position):-
 	blacksMovement(Values, Result, N, NextPosition).
 
 moveBlack(Result, Position, Value, N):-
-	NewPosition #= Position - N*Value,
-	element(NewPosition, Result, 1).
+	(NewPosition #= Position - N*Value),
+	element(NewPosition, Result, 1),
+	write(NewPosition),nl.
 moveBlack(Result, Position, Value, N):-
-	NewPosition #= Position + N*Value,
-	element(NewPosition, Result, 2).
-moveBlack(Result, Position, Value, N):-
-	NewPosition #= Position - Value,
-	element(NewPosition, Result, 3).
-moveBlack(Result, Position, Value, N):-
-	NewPosition #= Position + Value,
-	element(NewPosition, Result, 4).
+	(NewPosition #= Position + N*Value),
+	element(NewPosition, Result, 2),
+	write(NewPosition),nl.
+moveBlack(Result, Position, Value, _):-
+	(NewPosition #= Position - Value),
+	element(NewPosition, Result, 3),
+	write(NewPosition),nl.
+moveBlack(Result, Position, Value, _):-
+	(NewPosition #= Position + Value),
+	element(NewPosition, Result, 4),
+	write(NewPosition),nl.
 
 /*
 blacksMovement(ResultBoard, ValuesBoard):-
